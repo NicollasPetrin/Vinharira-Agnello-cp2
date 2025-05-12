21e7e4bd0b31124f5258774e083f8c8319ba6740
MarcoJunqueira <marcoalbuquerque2311@gmail.com>
2025-05-12 10:50:10 -0300

    Add files via upload
    implementação da function para validar entrada

--- src/js/script.js
+++ src/js/script.js
@@ -1,61 +1,13 @@
-// Pedindo os dados ao usuario
 function cadastrarVinho() {
-    var vinho = prompt("Digite o nome do vinho: ");
-        if (vinho == ""){
-            alert("O nome do vinho deve ser inserido !");
-            var vinho = prompt("Digite o nome do vinho: ");
+    
+    function validarEntrada(mensagem, tipo) {
+        let valor;
+        do {
+            valor = prompt(mensagem);
+            if (tipo === 'numero') {
+            valor = parseInt(valor);
+            }
+        } while (valor === null || valor === '' || isNaN(valor));
+        return valor;
         }
-    var tipo = prompt("Digite o tipo do vinho");
-        if (tipo == ""){
-            alert("O tipo do vinho deve ser inserido !");
-            var tipo = prompt("Digite o tipo do vinho: ");
-        }
-    var ano = prompt("Digite o ano do vinho: ");
-        if (ano == ""){
-            alert("O ano do vinho deve ser inserido!");
-            var ano = parseInt(prompt("Digite o ano do vinho: "));
-        }
-    var estoque = parseInt(prompt("Digite a quantidade de vinho: "));
-        if (estoque == ""){
-            alert("A quantidade de vinhos no estoque deve ser inserida!");
-            var vinho = parseInt(prompt("Digite a quantidade de garrafas de vinho: "));
-        }
-
-
-    // mostrar que o cadastro esta realizado e cadastro no console
-
-    alert(`Vinho cadastrado!!! \n \n \n Nome do vinho: ${vinho} \n Tipo do vinho: ${tipo} \n Estoque disponivel: ${estoque} \n Ano de fabricação: ${ano} `);
-    alert ("A seguir, veja os detalhes no console !");
-    console.log ("nome do vinho : " + vinho);
-    console.log ("tipo do vinho : " + tipo);
-
-
-    // avisos importantes no console
-
-    if(estoque < 5){
-        console.log ("estoque do vinho : " + estoque + "     ESTOQUE BAIXO");
-        if(ano >= 2020 ){
-            console.log("ano do vinho : " + ano + "      Vinho Jovem");
-        }
-        if(ano >= 2015 && ano <= 2019){
-            console.log("ano do vinho : " + ano + "     Vinho Amadurecido");
-        }
-        if(ano < 2015){
-            console.log("ano do vinho : " + ano + "     Vinho Antigo");
-        }
-        
-    }else {
-        console.log ("estoque do vinho : " + estoque);
-        if(ano >= 2020 ){
-            console.log("ano do vinho : " + ano + "      Vinho Jovem");
-        }
-        if(ano >= 2015 && ano <= 2019){
-            console.log("ano do vinho : " + ano + "     Vinho Amadurecido");
-        }
-        if(ano < 2015){
-            console.log("ano do vinho : " + ano + "     Vinho Antigo");
-        }
-    }
-
-    console.log("--------------------------------------------------------")
 }
